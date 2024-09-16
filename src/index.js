b6942a01b31811558e724f19fcc45d3b05ebf404
Yane Wanderley <yanewanderley@gmail.com>
2024-09-16 11:22:17 -0300

    Só para subir

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (request, response) => {
-    response.send('Oi mundao de Deus!');
+    response.send('Oi mundao de Deus sim!');
 });
 
 app.listen(3000, () => console.log('Server iniciou!'));
