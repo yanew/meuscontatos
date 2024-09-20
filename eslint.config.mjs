6a66bce06ccaa4622949c9ffb503aeaab58460cf
Yane Wanderley <yanewanderley@gmail.com>
2024-09-20 11:47:55 -0300

    Configuração do eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,5 +5,8 @@ import pluginJs from "@eslint/js";
 export default [
   {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
   {languageOptions: { globals: globals.node }},
+  {rules: {
+    "no-unused-vars": ['error', {argsIgnorePattern: 'next' }]
+  }},
   pluginJs.configs.recommended,
 ];
